39dc14282839c4a39f03de54ef1af6bbd326a2ea
Esemega <sogaral89@gmail.com>
2021-02-23 21:50:02 +0100

    Importing the json with fetch

--- JSON_tree/app.js
+++ JSON_tree/app.js
@@ -1,25 +1,26 @@
-const tree = {
-  tag: "ul",
-  children: [
-    {
-      text: "Primer elemento",
-    },
-    {
-      text: "Cáspita, otro elemento",
-    },
-    {
-      text: "El último",
-    },
-  ],
-};
-
-const convertJsonToHtml = (tree, indent = 'tab') => {
-    const indentString = indent === 'space' ? ' ' : "\t"
-
-    return `<${tree.tag}>
-${indentString}${tree.children.map(child => `<li>${child.text}</li>`).join("\n"+indentString)}
+/**
+ *  IDEAS
+ * 1) cargar el fichero json / fetch  -> https://stackoverflow.com/questions/34944099/how-to-import-a-json-file-in-ecmascript-6
+ * 2) TDD
+ * 3) funciones anidadas
+ * 4) documentar la función
+ * *Stringify -> https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/JSON/stringify
+ */
+//importing json file
+fetch('./tree.json')
+  .then(response => response.json())
+  .then(tree => {
+    console.log(tree)
+    
+    const convertJsonToHtml = (tree, indent = 'tab') => {
+      const childTag = 'li';
+      const indentString = indent === 'space' ? ' ' : "\t"
+      
+      return `<${tree.tag}>
+${indentString}${tree.children.map(child => `<${childTag}>${child.text}</${childTag}>`).join(`\n${indentString}`)}
 </${tree.tag}>`
-}
+    }
 
 
-console.log(convertJsonToHtml(tree, 'space'));
+    console.log(convertJsonToHtml(tree));
+})
