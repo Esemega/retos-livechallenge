e522789e404cfe40a7afdb13b5bee3097fe439f7
Esemega <sogaral89@gmail.com>
2021-02-23 23:18:21 +0100

    importing data asyncronusly, declaring the function outside of fetch and exporting the function to test it with jest

--- JSON_tree/app.js
+++ JSON_tree/app.js
@@ -1,26 +1,23 @@
-/**
- *  IDEAS
- * 1) cargar el fichero json / fetch  -> https://stackoverflow.com/questions/34944099/how-to-import-a-json-file-in-ecmascript-6
- * 2) TDD
- * 3) funciones anidadas
- * 4) documentar la función
- * *Stringify -> https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/JSON/stringify
- */
 //importing json file
-fetch('./tree.json')
-  .then(response => response.json())
-  .then(tree => {
-    console.log(tree)
-    
-    const convertJsonToHtml = (tree, indent = 'tab') => {
-      const childTag = 'li';
-      const indentString = indent === 'space' ? ' ' : "\t"
-      
-      return `<${tree.tag}>
+async function fetchData() {
+  let response = await fetch('./tree.json');
+  let data = await response.json();
+  return data;
+}
+
+const convertJsonToHtml = (tree, indent = 'tab') => {
+  const childTag = 'li';
+  const indentString = indent === 'space' ? ' ' : "\t"
+  
+  return `<${tree.tag}>
 ${indentString}${tree.children.map(child => `<${childTag}>${child.text}</${childTag}>`).join(`\n${indentString}`)}
 </${tree.tag}>`
-    }
+}
 
-
+fetchData()
+  .then(tree => {
+    console.log(tree)
     console.log(convertJsonToHtml(tree));
 })
+
+module.exports = convertJsonToHtml;
