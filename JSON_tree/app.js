367ed562be63e3dd69b14d15f493cce12ff52fde
Esemega <sogaral89@gmail.com>
2021-02-25 22:06:08 +0100

    getClass modified to be out of any function; hasChildren, getIndent and getChildren function created; getChildren is splited from convertJsonToHtml

--- JSON_tree/app.js
+++ JSON_tree/app.js
@@ -5,18 +5,43 @@ async function fetchData() {
   return data;
 }
 
-const convertJsonToHtml = (tree, indent = 'tab') => {
-  const childTag = 'li';
-  const indentString = indent.includes('space') ? ' '.repeat(indent.slice(5)) : "\t";
-  const getClass = element => element.class ? ` class='${element.class}'` : '';
+const getClass = element => element.class ? ` class='${element.class}'` : '';
+  
+const hasChildren = element => Boolean(element.children);
 
-  return `<${tree.tag}>
-${indentString}${tree.children.map(child => `<${childTag}${getClass(child)}>${child.text}</${childTag}>`).join(`\n${indentString}`)}
-</${tree.tag}>`
+const getIndent = (indent, level) => {
+  const indentString = indent.includes('space') ? ' '.repeat(indent.slice(5)) : "\t";
+  return indentString.repeat(level);
+}
+
+const getChildren = (tree, indent, level) => {
+  const childTag = 'li';
+
+  return tree.children.map( child => {
+    const result = `<${childTag}${getClass(child)}>${child.text}</${childTag}>`;
+    let childResult = '';
+    if (hasChildren(child)) {
+      childResult = `\n${child.children.map(grandchildren => `${convertJsonToHtml(grandchildren, indent, level + 1)}`)}`
+    }
+    return result  + childResult;
+  }).join(`\n${getIndent(indent, level + 1)}`)
+
+}
+
+const convertJsonToHtml = (tree, indent = 'tab', level = 0) => {
+  return `${getIndent(indent, level)}<${tree.tag}>
+${getIndent(indent, level + 1)}${getChildren(tree, indent, level)}
+${getIndent(indent, level)}</${tree.tag}>`
 }
 
 fetchData()
   .then( data => {    
+    console.log('tree', data.tree)
+    console.log(convertJsonToHtml(data.tree));
+
+    console.log('treeWithClass', data.treeWithClass)
+    console.log(convertJsonToHtml(data.treeWithClass));
+
     console.log('complexTree', data.complexTree)
     console.log(convertJsonToHtml(data.complexTree));
 
